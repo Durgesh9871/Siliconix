cacbf0fa8e4fcf2578c28e7fcd30ef721ed1bdca
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-18 19:18:04 +0530

    file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-<<<<<<< HEAD
+
 import "./App.css";
 import SearchBar from "./Pages/Manisha_Wadhe_Folder/SearchBar";
 // import AllRoutes from "./Routes/AllRoutes";
-function App() {
-  return (
-    <>
-      {/* <AllRoutes /> */}
-      <SearchBar/>
-    </>
-  );
-=======
 
 
 import Footer from './Pages/R_Sai_Kiran_Folder/Footer/Footer';
 import Homepage from './Pages/R_Sai_Kiran_Folder/Homepage/Homepage';
-import Navbar1 from './Pages/R_Sai_Kiran_Folder/Navber/Navbar1';
+// import Navbar1 from './Pages/R_Sai_Kiran_Folder/Navber/Navbar1';
 import "./Pages/R_Sai_Kiran_Folder/Navber/Navbar1.css"
-import Navbar2 from './Pages/R_Sai_Kiran_Folder/Navber/Navbar2';
+// import Navbar2 from './Pages/R_Sai_Kiran_Folder/Navber/Navbar2';
+
+
+// import { ProductPage } from "./Pages/Durgesh_Folder/Pages/ProductPage";
+// import { SinglePageProduct } from "./Pages/Durgesh_Folder/Pages/SinglePageProduct";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return(<>
 
   <Homepage/>
+  {/* <ProductPage /> */}
+  {/* <SinglePageProduct /> */}
+  {/* <AllRoutes /> */}
+
   </>)
->>>>>>> 8fecf5e2a2d80b7c5e5a9c09b77321e8260be549
+
 }
 
 export default App;
