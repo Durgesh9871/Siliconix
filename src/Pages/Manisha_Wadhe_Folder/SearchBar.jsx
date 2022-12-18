77f6d6bd8f28eec771f34e355c94c60136d20f04
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-18 20:59:30 +0530

    file

--- src/Pages/Manisha_Wadhe_Folder/SearchBar.jsx
+++ src/Pages/Manisha_Wadhe_Folder/SearchBar.jsx
@@ -1,6 +1,8 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import "./SearchBar.css";
+import { Box, SimpleGrid } from '@chakra-ui/react';
+import { DisplayProductMainData } from '../Durgesh_Folder/Components/DisplayProductMainData';
 
 const SearchBar = () => {
     const [loading, setLoading] = useState(false);
@@ -24,7 +26,18 @@ const SearchBar = () => {
         <div className='input'>
             <input type="text" placeholder='Search Products' onChange={(e)=> setSearchTitle(e.target.value)} />
        </div>
-        {loading? (<div className='img'><img src='https://media.tenor.com/wpSo-8CrXqUAAAAj/loading-loading-forever.gif'/></div>) : (
+       <Box border="2px solid red">
+      
+          <SimpleGrid columns={{base:1, sm: 1, md: 2, lg: 3,xl: 3,'2xl': 3,}} spacingY={10}  >
+           
+           {/* {productArrayLaptop.length > 0 && productArrayLaptop.map((item)=>{
+             return (
+           <DisplayProductMainData key= {item.id} id={item.id} src={item.Images} name={item.title} model={item.brand}    price={item.price}    review={item.rating}  realPrice={item.realPrice} isLaptopLoading={isLaptopLoading} allData ={item}/>
+           
+             )
+           })} */}
+
+{loading? (<div className='img'><img src='https://media.tenor.com/wpSo-8CrXqUAAAAj/loading-loading-forever.gif'/></div>) : (
             // post.map((item)=><h5 key={item.id}>{item.title}</h5>)
             post.filter((value)=>{
                 if(searchTitle === ""){
@@ -33,17 +46,29 @@ const SearchBar = () => {
                     return value;
                 }
             })
-            .map((item)=>
-            <div key={item.id}>
-                <img src={item.Images[0].imageFront}/>
-                <h5 >Name:{item.title}</h5>
-                 <h5>Discount:{item.discount}</h5>
-                 <h5>Price:{item.realPrice}</h5>
-                 <hr></hr>
+            .map((item)=>{
+                return (
+         <DisplayProductMainData key= {item.id} id={item.id} src={item.Images} name={item.title} model={item.brand}    price={item.price}    review={item.rating}  realPrice={item.realPrice} isLaptopLoading={true} allData ={item}/>
 
-            </div>
-            )
+                )
+            })
+            
         )}
+
+ {/* <div key={item.id}>
+            //     <img src={item.Images[0].imageFront}/>
+            //     <h5 >Name:{item.title}</h5>
+            //      <h5>Discount:{item.discount}</h5>
+            //      <h5>Price:{item.realPrice}</h5>
+            //      <hr></hr>
+
+            // </div> */}
+       
+       
+
+
+         </SimpleGrid>
+        </Box>
     </div>
   )
 }
